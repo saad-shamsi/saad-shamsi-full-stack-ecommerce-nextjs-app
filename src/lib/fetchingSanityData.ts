59764ed309bba72a12479f2d1e45dca7e012c253
Saad Shamsi <114074040+saad-shamsi@users.noreply.github.com>
2023-08-24 01:08:24 +0500

    added dynamic routes

---
+++ src/lib/fetchingSanityData.ts
@@ -0,0 +1,38 @@
+import { Image as IImage } from "sanity";
+import { urlForImage } from "../../sanity/lib/image";
+import { client } from "@/lib/client";
+const builder = imageUrlBuilder(client);
+import imageUrlBuilder from "@sanity/image-url";
+export interface IProduct {
+  title: string;
+  description: string;
+  image: IImage;
+  price: number;
+  _id: string;
+}
+export const getCardData = async () => {
+  const res = await fetch("http://localhost:3000/api/cart");
+  const data = await res.json();
+  return data;
+};
+
+function urlFor(source: any) {
+  return builder.image(source);
+}
+export async function getProductsData() {
+  const res = client.fetch(`*[_type=="product"]{
+      price,
+        _id,
+        product,
+        image,
+        alt,
+        category -> {
+          category
+    },
+    gender -> {
+      gender
+    }
+     
+    }`);
+  return res;
+}
